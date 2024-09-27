7634bfeb2c668a9f691800624ffbd055682be541
Project cua vu <lvu.byte@gmail.com>
2024-09-27 17:50:47 +0700

    update

--- lib/screens/item_truyen/view_screen/novel_detail_screen.dart
+++ lib/screens/item_truyen/view_screen/novel_detail_screen.dart
@@ -1,13 +1,9 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../../models/comment.dart';
 import '../../../models/novel.dart';
 import '../../../services/novel_service.dart';
-
-
 import '../../../widgets/general_widgets/cover_image.dart';
 import '../../../widgets/novel_widgets/chapter_list.dart';
 import '../../../widgets/novel_widgets/novel_info.dart';

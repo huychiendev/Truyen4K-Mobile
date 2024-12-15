b9695e3e9459d5c76d1f0ec8f46bc9c670b19db2
lvubyte <lvu.byte@gmail.com>
2024-12-15 20:38:00 +0700

    fix and optimize avatar comment_widget.dart, comment.dart

--- lib/widgets/comment/comment_widget.dart
+++ lib/widgets/comment/comment_widget.dart
@@ -3,7 +3,6 @@ import 'package:apptruyenonline/models/ProfileModel.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:apptruyenonline/models/ProfileModel.dart';
 import 'package:apptruyenonline/models/User.dart';
 import 'comment.dart';
 
